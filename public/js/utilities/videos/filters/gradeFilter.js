229beaaad4d29552ac4340703d2c14830822a7c5
Miriam Mae Iglesia <Yami@Miriams-MacBook-Pro.local>
2014-01-23 13:37:19 +0800

    Commit gradeFilter changes

--- public/js/utilities/videos/filters/gradeFilter.js
+++ public/js/utilities/videos/filters/gradeFilter.js
@@ -4,20 +4,32 @@ define(function (require) {
     var gradeFilter = require('utilities/videos/RecommendLogic');
     var utility      = require('utilities/videos/Utility');
 
+    function logger(sMsg) {
+      console.log(sMsg);
+    }
+
     gradeFilter.setExecuteMessage('Executing Grade Filter');
 
     gradeFilter.filter = function (videoData, filterdata, callback) {
 
       var arFilterGrade = filterdata['UserData'].gradelevel;
-      utility.filter(videoData, arFilterGrade, function( arResults ) {
 
-	//change videoData.raw reference to arHandler
-	videoData.raw = null;
-	videoData.raw = arResults;
+
+      utility.filter(videoData, arFilterGrade, function( err, arResults ) {
+
+        if(err) {
+          logger('Err: ' + err);
+          return callback(err);
+        }
+
+        logger('Fetched filtered data');
+
+        //change videoData.raw reference to arHandler
+        videoData.raw = null;
+        videoData.raw = arResults;
+        callback(videoData);
 
       });
-
-      callback(videoData);
 
     };
 
