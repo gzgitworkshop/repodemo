0d48528e349beae51e8ed474c9ce799fd48d43ed
Chad Dumadag <capdumadag@gmail.com>
2014-01-24 15:55:07 +0800

    Added Joseph's Select User Type UI.

--- public/js/utilities/videos/data/userFilterData.js
+++ public/js/utilities/videos/data/userFilterData.js
@@ -1,15 +1,20 @@
 define(function(require) {
     'use strict';
 
-    //sample filter data for subject
+    function getURLParameter(name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ""])[1].replace(/\+/g, '%20')) || null;
+    }
+
+    var role = getURLParameter('role');
+    role = role ? role + "/" : "";
 
     return function(callback) {
         $.ajax({
-            url: "http://localhost:8888/userData1.json",
+            url: "http://localhost:8888/" + role + "userData1.json",
             type: "GET",
             dataType: "json",
             success: function(data) {
-            	console.log(data);
+                console.log(data);
                 callback(data);
             },
             error: function(xhr, status, error) {
