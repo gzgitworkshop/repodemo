fd0cd2dc16a342e19f96128052e0dc523a88651b
shinji escorido <shinji.escorido@gmail.com>
2014-01-24 15:14:58 +0800

    TDD test for subject Filtering

--- test/public/spec/utilities/videos/filters/subjectFilterTest.js
+++ test/public/spec/utilities/videos/filters/subjectFilterTest.js
@@ -1,17 +1,16 @@
 define( function ( require ) {
-    'use strict';
+	'use strict';
 
-    var x = require('utilities/videos/filters/subjectFilter');
-    var models = {
+	var subjectFilter = require( 'utilities/videos/filters/subjectFilter' );
+	var models = {
         'VideoModel': require('models/videos/VideoModel')
     };
-
-    var videoData = {raw : [new models.VideoModel({
+var videoData = {raw : [new models.VideoModel({
             _id: 1,
             imageUrl: 'img/video-bg-2.png',
             topic: 'Closing the Gap',
             duration: '9 min',
-            tags: ['general', 'grade-1', 'English', 'Technology', 'grade-2']
+            tags: ['general', 'grade-1', 'English', 'Technology', 'grade-2', 'kindergarten', 'grade-7', 'grade-8', 'grade-9', 'grade-10', 'grade-11', 'grade-12']
         }), new models.VideoModel({
             _id: 2,
             imageUrl: 'img/video-bg-3.png',
@@ -41,30 +40,57 @@ define( function ( require ) {
             imageUrl: 'img/video-bg-7.png',
             topic: 'Learning Games',
             duration: '9 min',
-            tags: ['general', 'Technology','grade-1']
+            tags: ['general', 'Technology','grade-1','Math']
         }), new models.VideoModel({
             _id: 7,
             imageUrl: 'img/video-bg-8.png',
             topic: 'Communication',
             duration: '5 min',
-            tags: ['general', 'English', 'grade-1','grade-2', 'grade-3', 'grade-4', 'grade-5', 'grade-6','Pre-Kindergarten']
+            tags: ['general', 'English', 'grade-1','grade-2', 'grade-3', 'grade-4', 'grade-5', 'grade-6','prekindergarten']
         }), new models.VideoModel({
             _id: 8,
             imageUrl: 'img/video-bg-9.png',
             topic: 'Teaching and the Internet',
             duration: '11 min',
-            tags: ['general', 'Technology','grade-6','Pre-Kindergarten']
+            tags: ['general', 'Technology','grade-6','kindergarten', 'astronomy','construction','Foreign Language']
         })]};
+			
+			var subarray = ["English",
+			"Math",
+			"Foreign Language",
+			"Social Studies",
+			"Science",
+			"Performing Arts",
+			"Fine Arts",
+			"Music",
+			"Technology",
+			"Consumer science",
+			"Physical Education"];
 
-    var filterData = { UserData : { subject : [ 'Vocational' ], 
-                        gradelevel: [ 'grade-1', 'grade-3' ] 
-                     }};
+	  	describe( 'Subject Specific Content Filtering', function () {
+			subarray.forEach(function (subj){
+				describe( subj+' Teacher', function () {
 
-    describe( 'Subject Specific Content Filtering', function () {
+			            var grade = 'grade-6',
+			                filterData  = { UserData : { 
+			                                    subject : [ subj ]
+			                                }},
+			                video = videoData.raw;
+			          
+			            it('Should fetch '+subj+' Contents', function () {
+			                subjectFilter.filter(videoData, filterData, function(res){
+			                    if(res.raw) {
+			                       var obj = res.raw;
+			                        obj.forEach( function(val) {
+			                       console.log(val.attributes.tags);
+			                            (val.attributes.tags).should.contain(subj);                    
+			                        })                  
+			                    }                   
+			                });
+			            }); 
+			    } );
+			});
 
-        it('should have more test written', function () {
-            ( false ).should.be.ok;
-        } );
-    });
+		});
 
-});
+} );
