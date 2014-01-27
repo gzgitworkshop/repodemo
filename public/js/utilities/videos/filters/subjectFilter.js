fc783124f07cbdc64d8d442c13aae2d0a5897a21
Joseph Panuncillo <jpanuncillo@live.com>
2014-01-27 15:20:00 +0800

    enclosed logic with try-catch

--- public/js/utilities/videos/filters/subjectFilter.js
+++ public/js/utilities/videos/filters/subjectFilter.js
@@ -1,34 +1,39 @@
-define(function (require) {
+define(function(require) {
     'use strict';
 
-    var subjectFilter = new (require('utilities/videos/RecommendLogic'))();
-    var utility      = require('utilities/videos/Utility');
+    var subjectFilter = new(require('utilities/videos/RecommendLogic'))();
+    var utility = require('utilities/videos/Utility');
 
     function logger(sMsg) {
-      console.log(sMsg);
+        console.log(sMsg);
     }
 
     subjectFilter.setExecuteMessage('Executing Subject Filter');
 
-    subjectFilter.filter = function (videoData, filterData, callback) {
+    subjectFilter.filter = function(videoData, filterData, callback) {
 
-      var arFilterSubj = filterData['UserData'].subject;
+        var arFilterSubj = filterData['UserData'].subject;
 
-      utility.filter(videoData, arFilterSubj, 'subject' ,function( arResults ) {
+        try {
+            utility.filter(videoData, arFilterSubj, 'subject', function(arResults) {
 
-        if( !arResults ) {
-          return callback([]);
+                if (!arResults) {
+                    return callback([]);
+                }
+
+                logger('Fetched filtered data');
+
+                //change videoData.raw reference to arHandler
+                videoData.raw = null;
+                videoData.raw = arResults;
+
+                callback(videoData);
+
+            });
+        } catch (err) {
+            logger(err);
+            callback([]);
         }
-
-        logger('Fetched filtered data');
-
-        //change videoData.raw reference to arHandler
-        videoData.raw = null;
-        videoData.raw = arResults;
-
-        callback(videoData);
-
-      });
 
     };
 
