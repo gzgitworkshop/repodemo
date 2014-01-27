4797837b501f2bb6e9cfc753289e9fe7782a1b04
Jes Anub <mjanub@gmail.com>
2014-01-27 09:17:18 +0800

    Added test for multiple-grade-level filtering

--- test/public/spec/utilities/videos/filters/gradeFilterTest.js
+++ test/public/spec/utilities/videos/filters/gradeFilterTest.js
@@ -15,54 +15,79 @@ define( function ( require ) {
             imageUrl: 'img/video-bg-2.png',
             topic: 'Closing the Gap',
             duration: '9 min',
-            tags: ['general', 'English', 'Technology', 'grade-2', 'kindergarten', 'grade-7', 'grade-8', 'grade-9', 'grade-10', 'grade-11', 'grade-12']
+            tags: {
+                    subject    : [ "English", "Technology" ],
+                    gradelevel : [ "grade-1", "grade-2", "prekindergarten", "kindergarten" ]
+            }
         }), new models.VideoModel({
             _id: 2,
             imageUrl: 'img/video-bg-3.png',
             topic: 'Culture',
             duration: '3 min',
-            tags: ['general', 'Social Studies', 'grade-1']
+            tags: {
+                    subject    : [ "English", "Technology" ],
+                    gradelevel : [ "grade-1", "grade-2", "Kindergarten", "grade-12" ]
+            }
         }), new models.VideoModel({
             _id: 3,
             imageUrl: 'img/video-bg-4.png',
             topic: 'Phones in Class',
             duration: '4 min',
-            tags: ['general', 'Technology', 'Vocational', 'grade-6']
+            tags: {
+                    subject    : [ "English", "Technology" ],   
+                    gradelevel : [ "grade-1", "grade-2" ]
+            }
         }), new models.VideoModel({
             _id: 4,
             imageUrl: 'img/video-bg-5.png',
             topic: 'Time Management',
             duration: '7 min',
-            tags: ['general', 'grade-5', 'Business']
+            tags: {
+                    subject    : [ "English", "Technology" ],
+                    gradelevel : [ "grade-1", "grade-9", "kindergarten" ]
+            }
         }), new models.VideoModel({
             _id: 5,
             imageUrl: 'img/video-bg-6.png',
             topic: 'Tech Basics',
             duration: '4 min',
-            tags: ['general', 'Technology','grade-6']
+            tags: {
+                    subject    : [ "English", "Technology" ],
+                    gradelevel : [ "grade-1", "grade-7" ]
+            }
         }), new models.VideoModel({
             _id: 6,
             imageUrl: 'img/video-bg-7.png',
             topic: 'Learning Games',
             duration: '9 min',
-            tags: ['general', 'Technology','grade-1']
+            tags: {
+                    subject    : [ "English", "Technology" ],
+                    gradelevel : [ "grade-1", "grade-2" ]
+            }
         }), new models.VideoModel({
             _id: 7,
             imageUrl: 'img/video-bg-8.png',
             topic: 'Communication',
             duration: '5 min',
-            tags: ['general', 'English', 'grade-1','grade-2', 'grade-3', 'grade-4', 'grade-5', 'grade-6','prekindergarten']
+            tags: {
+                    subject    : [ "English", "Technology" ],
+                    gradelevel : [ "grade-1", "grade-2" ]
+            }
         }), new models.VideoModel({
             _id: 8,
             imageUrl: 'img/video-bg-9.png',
             topic: 'Teaching and the Internet',
             duration: '11 min',
-            tags: ['general', 'Technology','grade-6','kindergarten']
+            tags: {
+                    subject    : [ "English", "Technology" ],
+                    gradelevel : [ "grade-1", "grade-2" ]
+            }
         })]};
 
     describe( 'Grade Specific Content Filtering', function () {
 
-        var arrFilters = [  'Pre-Kindergarten',
+        var arrFilters = [  
+                            'Pre-Kindergarten',
                             'Kindergarten',
                             'Grade 1',
                             'Grade 2',
@@ -75,22 +100,28 @@ define( function ( require ) {
                             'Grade 9',
                             'Grade 10',
                             'Grade 11',
-                            'Grade 12' ];
+                            'Grade 12',
+                            'Instructional Coach 1',
+                            'Instructional Coach 2',
+                            'Instructional Coach - All' ];
         var tagFilters = [
-                            'prekindergarten',
-                            'kindergarten',
-                            'grade-1',
-                            'grade-2',
-                            'grade-3',
-                            'grade-4',
-                            'grade-5',
-                            'grade-6',
-                            'grade-7',
-                            'grade-8',
-                            'grade-9',
-                            'grade-10',
-                            'grade-11',
-                            'grade-12',
+                            ['prekindergarten'],
+                            ['kindergarten'],
+                            ['grade-1'],
+                            ['grade-2'],
+                            ['grade-3'],
+                            ['grade-4'],
+                            ['grade-5'],
+                            ['grade-6'],
+                            ['grade-7'],
+                            ['grade-8'],
+                            ['grade-9'],
+                            ['grade-10'],
+                            ['grade-11'],
+                            ['grade-12'],
+                            ['grade-1', 'prekindergarten'],
+                            ['grade-2', 'grade-1', 'grade-10', 'kindergarten', 'grade-12'],
+                            ['kindergarten', 'prekindergarten', 'grade-1', 'grade-2', 'grade-3', 'grade-4', 'grade-5', 'grade-6', 'grade-7', 'grade-8', 'grade-9', 'grade-10', 'grade-11', 'grade-12']
                         ];
 
         arrFilters.forEach(function (item) {
@@ -102,52 +133,63 @@ define( function ( require ) {
 
                 var grade = tagFilters[arrFilters.indexOf(item)],
                         filterData  = { UserData : { 
-                                            subject : [ 'general' ], 
-                                            gradelevel: [ grade ] 
+                                            subject : [  ], 
+                                            gradelevel: grade 
                                         }},
                         video = copyObject(videoData),
                         counttags = 0,
                         fetchedtags = 0,
                         videoraw = video.raw;
                 var videoraw = video.raw;
-                it(strdes1, function (done) {
+                it(strdes1, function () {
                     if(video) {
-                        console.log(videoData);
-                        console.log('count video: ' + videoraw.length);
+
                         videoraw.forEach ( function(val) {
-                            if( val.attributes.tags.indexOf(grade) > -1 ) {
-                                counttags++;
-                            }    
+                            //loop on all values of tagfilters
+                            grade.every ( function (fil)  {
+                                if( val.attributes.tags.gradelevel.indexOf(fil) > -1 ) {
+                                    counttags++;
+                                    return false;
+                                } else {
+                                    return true;
+                                }
+                            })
                         })
-                        console.log('available items on stack: ' + counttags);
-                    }
+                        logger('available items on stack: ' + counttags);
+                    } 
                     
                     gradeFilter.filter(video, filterData, function(res){
+                        logger(res);
                         if(res.raw) {
                             fetchedtags = res.raw.length;
-                            console.log('Fetched items: ' + fetchedtags);
+                            logger('Fetched items: ' + fetchedtags);
                             var obj = res.raw;
                             obj.forEach( function(val) {
-                                console.log('Fetched data: ' + val.attributes.tags);
-                                (val.attributes.tags).should.contain(grade);
-                                console.log(val);
+                                var isExist = false;
+                                grade.every ( function (fil)  {
+                                    if ( val.attributes.tags.gradelevel.indexOf(fil) > -1 ) {
+                                        isExist = true;
+                                        return false;
+                                    } else {
+                                        return true;
+                                    }
+                                })
+                                isExist.should.be.true;
                             })
                         }
                     });
-                    done();
+                    
                 });
 
-                it(strdes2, function (done) {
+                it(strdes2, function () {
                     counttags.should.be.equal(fetchedtags);
-                    console.log(videoData);
-                    done();
+                    logger(videoData);
                 });
 
             } );
         });
 
     });
-
 
     function copyObject(obj) {
         var newObj = {};
@@ -156,6 +198,10 @@ define( function ( require ) {
             newObj[key] = obj[key];
         }
         return newObj;
+    }
+
+    function logger ( msg ) {
+        console.log ( msg );
     }
 
 });
