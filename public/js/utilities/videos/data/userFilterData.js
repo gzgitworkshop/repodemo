08bfbb61b1b372e3e0cd3f205a0dcbcf056524c1
Joseph Panuncillo <jpanuncillo@live.com>
2014-01-28 11:57:26 +0800

    added check box roles

--- public/js/utilities/videos/data/userFilterData.js
+++ public/js/utilities/videos/data/userFilterData.js
@@ -5,20 +5,57 @@ define(function(require) {
         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ''])[1].replace(/\+/g, '%20')) || null;
     }
 
-    var role = getURLParameter('role');
-    role = role ? role + '/' : '';
+    // var role = getURLParameter('role');
+    // role = role ? role + '/' : '';
+
+    // return function(callback) {
+    //     $.ajax({
+    //         url: 'http://zubu.cloudapp.net:8888/' + role + 'userData1.json?ts=' + (new Date().getTime()),
+    //         type: 'GET',
+    //         dataType: 'json',
+    //         success: function(data) {
+    //             callback(data);
+    //         },
+    //         error: function(xhr, status, error) {
+    //             callback([]);
+    //         }
+    //     });
+    // };
+
+    var demoObj = {
+        'subject': [],
+        'gradelevel': []
+    };
 
     return function(callback) {
         $.ajax({
-            url: 'http://zubu.cloudapp.net:8888/' + role + 'userData1.json?ts=' + (new Date().getTime()),
-            type: 'GET',
-            dataType: 'json',
+            url: "http://zubu.cloudapp.net:8888/subjects.json?ts=" + (new Date().getTime()),
+            type: "GET",
+            dataType: "json",
             success: function(data) {
-                callback(data);
+                var sub = getURLParameter('sub');
+                var gra = getURLParameter('gra');
+
+                //for demo only eval() will be removed
+                if (sub) {
+                    eval("demoObj.subject = " + sub + ";");
+                }
+                if (gra) {
+                    eval("demoObj.gradelevel = " + gra + ";");
+                }
+
+                for (var i in demoObj.subject) {
+                    demoObj.subject[i] = data.subjects[demoObj.subject[i]].toLowerCase();
+                }
+                for (var i in demoObj.gradelevel) {
+                    demoObj.gradelevel[i] = data.grades[demoObj.gradelevel[i]].toLowerCase();
+                }
+
+                console.log(demoObj);
+
+                callback(demoObj);
             },
-            error: function(xhr, status, error) {
-                callback([]);
-            }
+            error: function(err) {}
         });
-    };
+    }
 });
